a8f42a64f9a3b708e0f916d969d419dc1ab8904f
cyan <kartggb@gmail.com>
2025-04-07 20:35:02 +0400

    Update

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
